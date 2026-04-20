a0a8e20eda43a4db49859e6b5bac15a6e5785a5d
nguyenquangduy9825 <20233525@eaut.edu.vn>
2026-04-20 10:09:05 +0700

    tạo class sinh viên

---
+++ sinhvien.js
@@ -0,0 +1,24 @@
+class sinhvien{
+    constructor(ten,tuoi,diachi,masv,diem=[]){
+        this.ten=ten;
+        this.tuoi=tuoi;
+        this.diachi=diachi;
+        this.masv=masv;
+        this.diem=diem;
+    }
+    xeploai(){
+        if(this.diem>=8){
+            return "giỏi"
+        }
+        if(this.diem>=6,5){
+            return "khá"
+        }
+        if(this.diem>=5){
+            return "trung bình"
+        }
+        else{
+            return "yếu "
+        }
+
+    }
+}
