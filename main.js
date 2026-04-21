9b7b4a86e16ad307a15b5cbaea0c3347b3b2bfb8
nguyenquangduy9825 <20233525@eaut.edu.vn>
2026-04-21 21:54:30 +0700

    xóa file main cho cả vào file html

--- main.js
+++
@@ -1,8 +0,0 @@
-import quanLy from "./quanly.js";
-import { dataSV } from "./data.js";
-
-let quanly=new quanly();
-for (let i=0;i<dataSV.length;i++){
-quanly.themsv(dataSV[i]);
-}
-console.log(dataSV[i]);
