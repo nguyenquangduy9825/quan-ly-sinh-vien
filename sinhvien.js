add0ab69469a098fd6739c703c8fe5c77e901cef
nguyenquangduy9825 <20233525@eaut.edu.vn>
2026-04-22 12:58:47 +0700

    sửa lại class sinh viên

--- sinhvien.js
+++ sinhvien.js
@@ -6,19 +6,5 @@
         this.masv=masv;
         this.diem=diem;
     }
-    xeploai(){
-        if(this.diem>=8){
-            return "giỏi"
-        }
-        else if(this.diem>=6.5){
-            return "khá"
-        }
-        else if(this.diem>=5){
-            return "trung bình"
-        }
-        else{
-            return "yếu "
-        }
 
     }
-}
